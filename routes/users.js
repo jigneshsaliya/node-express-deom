65d87896b1108716bb1c8cf61da797b51f1dea52
Jignesh Saliya <Jignesh.Slayia@jetblue.com>
2015-07-31 18:44:50 +0530

    Delete and update user service added

--- routes/users.js
+++ routes/users.js
@@ -30,16 +30,32 @@ router.post('/adduser',function(req,res,next){
 router.put('/updateuser',function(req,res,next){
 	var db = req.db;
 	var users = db.get('users');
-	var userId = (req.body['_id']).toString();
+	 console.log(req.body['_id']);
+	var userId = req.body['_id'];
 	var userName = req.body['userName'];
 	var password = req.body['password'];
 	var userEmail = req.body['email'];
-	console.log(req.body);
-	users.updateById('_id', {_id:userId,userName:userName,password:password,email:userEmail}, function (err, doc) {
+	
+	users.update({}, {_id:userId,userName:userName,password:password,email:userEmail}, function (err, doc) {
 	  if (err) throw err;
 	});
 	res.status(200).json({ message: 'user updated successfully.' })
 	res.end();
+});
+
+router.put('/deleteUser/:id',function(req,res,next){
+	var db = req.db;
+	var users = db.get('users');
+	var userId = req.params.id;
+	console.log(userId);
+
+	users.remove({_id: userId }, function (err) {
+	  if (err) throw err;
+	});
+
+	res.status(200).json({ message: 'user deleted successfully.' })
+	res.end();
+
 });
 
 
